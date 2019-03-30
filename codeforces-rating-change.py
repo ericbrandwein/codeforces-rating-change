831f7048c2eda45161f4769f692de99e44ddd5a0
Eric Brandwein <brandweineric@gmail.com>
2019-03-30 16:20:20 -0300

    Adjusting for inflation
    It now has to calculate all rating changes before calculating the
    final rating change. It is very slow, and it doesn't even give
    the right answer, maybe because the rating algorithm has changed
    since October 2015, or maybe because this has a bug.

--- codeforces-rating-change.py
+++ codeforces-rating-change.py
@@ -24,71 +24,93 @@ class Bounds:
     def get_middle(self):
         return (self.upper_bound + self.lower_bound) / 2
 
-    def same_bounds(self):
-        return self.lower_bound == self.upper_bound
+
+class Competitor:
+    def __init__(self, handle, actual_rank):
+        self.handle = handle
+        self.actual_rank = actual_rank
+        self.expected_rank = None
+        self.old_rating = None
+        self.rating_change = None
 
 
-def search_rating_for(
-        rating_bounds, desired_standing, participant_ratings):
-    middle_rating = ceil(rating_bounds.get_middle())
-    if rating_bounds.same_bounds():
-        found_rating = middle_rating
-    else:
+def adjust_total_sum_zero(competitors):
+    """
+    Total sum of rating changes should not be more than zero.
+    """
+    rating_changes = [
+        competitor.rating_change for competitor in competitors
+    ]
+    increment = int(-sum(rating_changes) / len(rating_changes) - 1)
+    for competitor in competitors:
+        competitor.rating_change += increment
+
+
+def adjust_sum_of_top_zero(competitors):
+    """
+    Sum of top 4*sqrt ratings should be adjusted to zero.
+    """
+    competitors.sort(key=lambda comp: comp.old_rating, reverse=True)
+    list_length = len(competitors)
+    number_of_contestants_on_top = int(min(
+        4 * round(sqrt(list_length)),
+        list_length
+    ))
+    rating_change_sum = 0
+    for competitor in competitors[:number_of_contestants_on_top]:
+        rating_change_sum += competitor.rating_change
+
+    increment = int(min(
+        max(-rating_change_sum / number_of_contestants_on_top, -10),
+        0
+    ))
+    for competitor in competitors:
+        competitor.rating_change += increment
+
+
+def adjust_rating_changes_for_inflation(competitors):
+    adjust_total_sum_zero(competitors)
+    adjust_sum_of_top_zero(competitors)
+
+
+def search_rating_for(competitor_ratings, desired_standing, rating_bounds):
+    while rating_bounds.upper_bound - rating_bounds.lower_bound > 1:
+        middle_rating = ceil(rating_bounds.get_middle())
         expected_standing = get_expected_standing(
-            middle_rating, participant_ratings)
-
-        if expected_standing == desired_standing:
-            found_rating = middle_rating
+            middle_rating, competitor_ratings)
+        if expected_standing < desired_standing:
+            # If the standing is lower in number than the one desired,
+            # the rating should be lower.
+            rating_bounds.upper_bound = middle_rating
         else:
-            if expected_standing < desired_standing:
-                # If the standing is lower in number than the one desired,
-                # the rating should be lower.
-                rating_bounds.upper_bound = middle_rating - 1
-            else:
-                # expected_standing > desired_standing
-                # If the standing is higher in number than the one desired,
-                # the rating should be higher.
-                if middle_rating == rating_bounds.upper_bound:
-                    rating_bounds.lower_bound = middle_rating
-                else:
-                    rating_bounds.lower_bound = middle_rating + 1
-            found_rating = search_rating_for(
-                rating_bounds, desired_standing, participant_ratings)
+            # expected_standing > desired_standing
+            # If the standing is higher in number than the one desired,
+            # the rating should be higher.
+            rating_bounds.lower_bound = middle_rating
 
-    return found_rating
+    return rating_bounds.lower_bound
 
 
-# El rating se está calculando mal porque no estoy tomando en
-# cuenta el ajuste que se hace para que no haya inflación.
-# El código en C++ está en https://codeforces.com/contest/1/submission/13861109,
-# en la función process().
-def get_expected_rating_change(
-        expected_standing, actual_standing, previous_rating, participant_ratings):
-    mean_standing = floor(sqrt(expected_standing * actual_standing))
-    if mean_standing < actual_standing:
+def rating_change_from_standing(competitor, competitors):
+    mean_standing = floor(
+        sqrt(competitor.expected_rank * competitor.actual_rank))
+
+    if mean_standing < competitor.actual_rank:
         other_bound = 0
     else:
         other_bound = MAX_RATING
-    rating_search_bounds = Bounds(other_bound, previous_rating)
+    rating_search_bounds = Bounds(other_bound, competitor.old_rating)
 
+    competitors_ratings = [
+        competitor.old_rating for competitor in competitors
+    ]
     calculated_rating = search_rating_for(
-        rating_search_bounds, mean_standing, participant_ratings)
+        competitors_ratings, mean_standing, rating_search_bounds)
 
-    expected_rating_change = int((calculated_rating - previous_rating) / 2)
+    expected_rating_change = int(
+        (calculated_rating - competitor.old_rating) / 2)
 
     return expected_rating_change
-
-
-def get_current_participant_rating(handle):
-    participant_rating_changes = list(api.user_rating(handle))
-    rating = 0
-    if participant_rating_changes:
-        rating = participant_rating_changes[-1].new_rating
-    return rating
-
-
-def handle_of_row(row):
-    return row.party.members[0].handle
 
 
 def probability_of_greater_rank(first_rating, second_rating):
@@ -96,91 +118,119 @@ def probability_of_greater_rank(first_rating, second_rating):
     return 1 / (1 + 10 ** (rating_difference / 400))
 
 
-def get_expected_standing(my_rating, participant_ratings):
+def get_expected_standing(my_rating, competitor_ratings):
     expected_standing = 1
-    for other_rating in participant_ratings:
+    for other_rating in competitor_ratings:
         expected_standing += probability_of_greater_rank(
             other_rating, my_rating)
 
     return expected_standing
 
 
-def retrieve_ratings_from_current_ratings(api, handles, my_handle):
+def get_expected_competitor_standing(competitor, other_competitors):
+    competitors_ratings = [
+        competitor.old_rating for competitor in other_competitors
+    ]
+    return get_expected_standing(competitor.old_rating, competitors_ratings)
+
+
+def load_rating_changes_from_old_ratings(competitors):
+    for index, competitor in enumerate(competitors):
+        competitors_without_current = [
+            competitors[i] for i in range(len(competitors))
+            if i != index
+        ]
+        competitor.expected_rank = get_expected_competitor_standing(
+            competitor, competitors_without_current)
+        competitor.rating_change = rating_change_from_standing(
+            competitor, competitors)
+
+    return adjust_rating_changes_for_inflation(competitors)
+
+
+def retrieve_ratings_from_current_ratings(api, competitors):
     # We have to split the handles in blocks of 500 because otherwise
     # the API explodes.
     ratings = []
-    for i in range(0, len(handles), 500):
+    increase = 500
+    for i in range(0, len(competitors), increase):
+        current_handles = [
+            competitor.handle for competitor in competitors[i:i + increase]
+        ]
         ratings += [
-            user.rating for user in api.user_info(handles[i:i + 500])
+            user.rating for user in api.user_info(current_handles)
         ]
 
-    my_rating = get_current_participant_rating(my_handle)
-    return my_rating, ratings
+    return ratings
 
 
-def retrieve_ratings_from_rating_changes(api, contest_id, my_handle):
+def retrieve_ratings_from_rating_changes(api, contest_id):
     rating_changes = api.contest_rating_changes(contest_id)
     rating_changes_list = list(rating_changes)
     my_rating = None
     previous_ratings = []
     for rating_change in rating_changes_list:
-        if rating_change.handle == my_handle:
-            my_rating = rating_change.old_rating
-        else:
-            previous_ratings.append(rating_change.old_rating)
-    return my_rating, previous_ratings
+        previous_ratings.append(rating_change.old_rating)
+    return previous_ratings
+
+
+def load_old_ratings(api, contest, competitors):
+    """
+    Loads old_rating from the api into competitors.
+    """
+    if contest.phase == ContestPhase.finished:
+        competitors_ratings = retrieve_ratings_from_rating_changes(
+            api, contest.id)
+    else:
+        competitors_ratings = retrieve_ratings_from_current_ratings(
+            api, competitors)
+
+    for index, competitor in enumerate(competitors):
+        competitor.old_rating = competitors_ratings[index]
+
+
+def calculate_rating_changes(contest_id):
+    api = CodeforcesAPI()
+    print('Retrieving standings...')
+    standings = api.contest_standings(contest_id)
+    print('Standings retrieved!')
+
+    competitors = [
+        Competitor(row.party.members[0].handle, row.rank)
+        for row in standings['rows']
+    ]
+    contest = standings['contest']
+
+    print('Retrieving ratings...')
+    load_old_ratings(api, contest, competitors)
+    print('Ratings retrieved!')
+
+    load_rating_changes_from_old_ratings(competitors)
+    return competitors
 
 
 contest_id = int(sys.argv[1])
 handle = sys.argv[2]
 
-api = CodeforcesAPI()
-print('Retrieving standings...')
-standings = api.contest_standings(contest_id)
-print('Standings retrieved!')
-
-participants = list(standings['rows'])
-
-print('Retrieving ratings...')
-contest = standings['contest']
-if contest.phase == ContestPhase.finished:
-    my_rating, participant_ratings = retrieve_ratings_from_rating_changes(
-        api, contest_id, handle)
-else:
-    participant_handles = [
-        handle_of_row(participant) for participant in participants
-        if handle_of_row(participant) != handle
-    ]
-    my_rating, participant_ratings = retrieve_ratings_from_current_ratings(
-        api, participant_handles, handle)
-print('Ratings retrieved!')
-
-
-expected_standing = get_expected_standing(my_rating, participant_ratings)
-
-actual_standing = [
-    participant.rank for participant in participants if handle_of_row(participant) == handle
+competitors = calculate_rating_changes(contest_id)
+my_competitor = [
+    competitor for competitor in competitors if competitor.handle == handle
 ][0]
-
-rating_change = get_expected_rating_change(
-    expected_standing, actual_standing, my_rating, participant_ratings)
 
 GREEN_TEXT_BLOCK = '\033[1;32;40m'
 RED_TEXT_BLOCK = '\033[1;31;40m'
 END_BLOCK = '\033[1;0;0m'
 
-if rating_change >= 0:
-    rating_change_string = '+' + str(rating_change)
-    if rating_change > 0:
+if my_competitor.rating_change >= 0:
+    rating_change_string = '+' + str(my_competitor.rating_change)
+    if my_competitor.rating_change > 0:
         rating_change_string = GREEN_TEXT_BLOCK + rating_change_string + END_BLOCK
 else:
-    rating_change_string = RED_TEXT_BLOCK + str(rating_change) + END_BLOCK
+    rating_change_string = RED_TEXT_BLOCK + \
+        str(my_competitor.rating_change) + END_BLOCK
 
-print('Expected standing: {}'.format(expected_standing))
-print('Actual standing: {}'.format(actual_standing))
+print('Expected standing: {}'.format(my_competitor.expected_rank))
+print('Actual standing: {}'.format(my_competitor.actual_rank))
 print('Expected rating change: {} ({} -> {})'.format(
-    rating_change_string, my_rating, my_rating + rating_change))
-if actual_standing <= expected_standing:
-    print("You did well!")
-else:
-    print("You did badly :(")
+    rating_change_string, my_competitor.old_rating,
+    my_competitor.old_rating + my_competitor.rating_change))
